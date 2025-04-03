38a1b1d0a3dee0acabf782e6fdca0e8c2fdd9166
MajidAbdelilah <abdelilahmaid012@gmail.com>
2025-04-03 01:31:54 +0000

    init

--- ex00/Main.java
+++ ex00/Main.java
@@ -1,1 +1,15 @@
-ls
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int val = 479598;
+        int tmp = val;
+        int res = 0;
+        while(tmp > 0)
+        {
+            res += tmp % 10;
+            tmp /= 10;
+        }
+        System.out.println(res);
+    }
+}
